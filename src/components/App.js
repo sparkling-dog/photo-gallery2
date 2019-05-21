6172146949f3a80e5a4251f0b6e47b88f76ec219
DannySuarez <suarezd10@gmail.com>
2019-05-21 16:03:50 -0700

    add BuildingList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import Component from './Component.js';
 import Header from './Header.js';
+import BuildingList from './BuildingList.js';
 
 class App extends Component {
     render() {
         const dom = this.renderDOM();
 
+        const main = dom.querySelector('main');
+
         const header = new Header();
         const headerDOM = header.render();
-
-        const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
+
+        const buildingList = new BuildingList();
+        const buildingListDOM = buildingList.render();
+        main.appendChild(buildingListDOM);
 
         return dom;
     }
@@ -18,7 +23,6 @@ class App extends Component {
         return /*html*/`
             <div>
                 <main>
-                <!-- gallery -->
                 </main>
             </div>
         `;
---
+++ src/components/BuildingList.js
@@ -0,0 +1,14 @@
+import Component from './Component.js';
+
+class BuildingList extends Component {
+    
+
+    renderTemplate() {
+        return /*html*/`
+            <ul id="buildings">Word</ul>
+        `;
+    }
+
+}
+
+export default BuildingList;
