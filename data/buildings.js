f64d5074dbbb53538b6b677f3c8acd57cf23528b
DannySuarez <suarezd10@gmail.com>
2019-05-21 15:03:55 -0700

    initialized starter files

---
+++ data/buildings.js
@@ -0,0 +1,35 @@
+const buildings = [
+    {
+        src: './assets/Acropolis.jpg',
+        title: 'Acropolis',
+        description: 'Ancient greek building from 19th century b.c.',
+        keyword: 'acropolis'
+    },
+    {
+        src: './assets/Great-Temple-of-Amun.jpg',
+        title: 'Great-Temple-of-Amun',
+        description: 'Ancient Egyption building from 1530 b.c.',
+        keyword: 'amun'
+    },
+    {
+        src: './assets/Machu-Picchu.jpg',
+        title: 'Machu-Picchu',
+        description: 'Ancient Peruvian building 15th century a.d.',
+        keyword: 'picchu'
+    },
+    {
+        src: './assets/Mosque-at-Djenne.jpg',
+        title: 'Mosque-at-Djenne',
+        description: 'Old African building 14th century a.d.',
+        keyword: 'djenne'
+    },
+    {
+        src: './assets/Petra.jpg',
+        title: 'Petra',
+        description: 'Ancient Jordanian building from 1st century a.d.',
+        keyword: 'petra'
+    }
+
+];
+
+export default buildings;
